447d6c835bbfdf5592889e7520da0abe3709e8f2
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:49:44 -0600

    Add conditional greeting based on user input

---
+++ práctica 5/condicionales.py
@@ -0,0 +1,12 @@
+#Logico booleano bool
+#Verdadero / falso: True /false
+#dato = 5 < 5
+#< > ==
+
+print("Ingesa tu nombre")
+nombre = input()
+
+if nombre == "Melissa":
+    print("Bienvenido Melissa")
+else:
+    print("Que nombre tan extraño")

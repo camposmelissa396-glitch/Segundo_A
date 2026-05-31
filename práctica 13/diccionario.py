51c805719d3b24a1fd57f42d66bc2d500b9c84e1
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:58:28 -0600

    Create diccionario.py with person dictionary
    Add a dictionary to store person details with key-value pairs.

---
+++ práctica 13/diccionario.py
@@ -0,0 +1,14 @@
+#Lista de elementos llave:valor
+#Arreglos asociativos
+
+persona = {"nombre": "Rodrigo",
+           "edad": 800,
+           "apellido": "Montemayor"}
+
+persona["apodo"] = "Ringa tech"
+
+#print(persona.items())
+
+print("dirveccion" in persona)
+
+#for key, value in persona.items():

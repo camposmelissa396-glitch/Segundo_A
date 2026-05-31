aaa99b7bf028a81832d4589f08def0047421ce87
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:52:13 -0600

    Add while loop and user input options
    Implement while and for loops with user input handling.

---
+++ práctica 7/ciclos.py
@@ -0,0 +1,32 @@
+#ciclo, iteracion bucle
+
+#while
+i = 0
+while i < 10:
+    if i < 5:
+        print("El numero", i, "es mayor a 5")
+else:
+    print("El numero", i, "es mayor o igual a 5")
+
+    i + 1
+
+    print("Termino la iteracion")
+
+
+    # for x in range (1, 11):
+            #print(x)
+  while True:
+   print("Escribe la opcion deseada")
+   print("1, saludar")
+   print("2. salir")
+
+
+
+   respuesta = int(input())
+
+   if respuesta == 1:
+       print("saludos terricola!")
+   elif respuesta == 2:
+       break
+   
+   print("Terminado programa")

c1d114443b278159a1d84d235c99827fb31504d2
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:51:12 -0600

    Add conditional greetings based on user name

---
+++ práctica 6/condicionales2.py
@@ -0,0 +1,13 @@
+print("Escribe tu nombre:")
+nombre = input()
+
+#elif
+
+
+if nombre == "Melissa":
+    print("Saludo Melissa")
+elif nombre =="Lourdes":
+    print("Que bonito nombre lourdes")
+
+else:
+    print("Que extraño nombre")

d80803615a0dbabc47f84a6ec40d732dfb59839e
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:46:07 -0600

    Add tienda.py for store welcome and product total

---
+++ práctica 2/tienda.py
@@ -0,0 +1,14 @@
+print("***************************")
+print("**      BIENVENIDO A     **")
+print("** LA TIENDA DE MELISSA **")
+print("***************************")
+
+num_churros = 2
+num_soda = 1.75
+num_paletas = 2
+
+Producto_totales = num_churros + num_soda + num_paletas
+
+print("Actualmente contamos con:")
+print("churros:", num_churros, "soda:", num_soda, "peletas", num_paletas)
+print("En total tenemos", Producto_totales,"producto")

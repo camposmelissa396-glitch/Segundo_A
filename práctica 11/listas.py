a49fa09fe4d7914119e34fc8e78be7d978af4f61
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:56:24 -0600

    Add names list and welcome messages in listas.py

---
+++ práctica 11/listas.py
@@ -0,0 +1,9 @@
+ombre = ["Rodrigo", "Pedro", "Santiago", "Jorge", "Raymundo"]
+print(nombre)
+#if-strings
+for i, nombres in enumerate (nombre):
+    #print("Se inscribe", i "en la lista"i, nombre)
+    print(f"se inscribio {nombres} en la lista con el indice {i}")
+
+    print("Bienveido a la fiesta", nombres[:3])
+    print("Lo siento", nombres[:3])

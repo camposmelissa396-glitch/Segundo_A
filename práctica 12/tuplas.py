9f45bad90e78220e2d9c56e7f749d9efb57a1738
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:57:35 -0600

    Create tuplas.py to explain tuples in Python
    Added a new Python file explaining tuples and their usage.

---
+++ práctica 12/tuplas.py
@@ -0,0 +1,12 @@
+Tuplas my similares a alas listas
+#No pueden modificarse
+#se utilizan parentesis en lugar de []
+
+listas = ["Rodrigo", "Juan"] #Constantemente agregado informacion
+tupla = ("Rodrigo", "Juan")
+
+videojuego = []
+#Nombres, Genero, Años
+videojuego.append( ("Final Fantasy 7", "JRPG", 1997) )
+videojuego.append( ("Outer wilds", "Aventura", 2019) )
+print(videojuego)#

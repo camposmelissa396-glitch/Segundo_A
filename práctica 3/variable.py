68b89ab54ea61cb8c63da50c1a9b25e7d4ea7370
camposmelissa396-glitch <camposmelissa396@gmail.com>
2026-05-31 12:47:26 -0600

    Add variable definitions and print statements

---
+++ práctica 3/variable.py
@@ -0,0 +1,14 @@
+#soy un comentario
+#strings, cadenas, textos 
+#numericos int (integer, entero) float (punto flotante)
+nombre = "Melissa"
+apellido = "Campos"
+edad:"17"
+
+print(nombre, apellido, edad)
+
+#edad = edad /3
+
+print(nombre, apellido, edad)
+
+print(type(edad))
